e749053bc71805e38db08e79b91bdf0e3477bee2
asma <as.ch.2280@gmail.com>
2022-02-15 14:13:08 +0330

    Add Loign and Register

---
+++ src/Login.js
@@ -0,0 +1,42 @@
+import React from "react"
+import axios from "axios";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link
+} from "react-router-dom";
+class Login extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state={user:{email:"",password:""}}
+        this. handleSubmit = this. handleSubmit.bind(this);
+    }
+    handleSubmit(event)
+    {
+        event.preventDefault()
+        this.setState(state=>{const user={email:this.element.value,password: this.element1.value};
+        return user;})
+        // axios.post("",this.state.user)
+        //     .then(res=>{console.log(res)})
+        //     .catch(res=>{console.log(res)})
+    }
+    render() {
+        return(
+            <div className="Login">
+
+                        <ul>
+                              <li><Link to="/Login">Login</Link></li>
+                            <li><Link to="/Register">Register</Link></li>
+                        </ul>
+
+            <form  className="Login-form" action="" onSubmit={this.handleSubmit}>
+                <input  className="Login-email" type="email" placeholder="Email" ref={el => this.element = el}/>
+                <input className="Login-password" type="password" placeholder="Password" ref={el => this.element1 = el}/>
+                <button  className="Login-button" type="submit">Login</button>
+            </form>
+            </div>
+        )
+    }
+}
+export  default Login;
---
+++ src/Register.js
@@ -0,0 +1,43 @@
+import React from "react";
+import axios from "axios";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link
+} from "react-router-dom";
+class Register extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state={user:{name:"",email:"",password:""}}
+        this. handleSubmit = this. handleSubmit.bind(this);
+    }
+    handleSubmit(event)
+    {
+        event.preventDefault()
+        this.setState(state=>{const user={name:this.element.value,email:this.element1.value,password: this.element2.value};
+            return user;})
+        // axios.post("",this.state.user)
+        //     .then(res=>{console.log(res)})
+        //     .catch(res=>{console.log(res)})
+    }
+    render() {
+        return(
+            <div className="Register">
+
+                    <ul>
+                        <li><Link to="/Login">Login</Link></li>
+                        <li><Link to="/Register">Register</Link></li>
+                    </ul>
+
+                <form  className="Register-form" action="" onSubmit={this.handleSubmit}>
+                    <input  className="Register-email" type="text" placeholder="Name" ref={el => this.element = el}/>
+                    <input  className="Register-email" type="email" placeholder="Email" ref={el => this.element1 = el}/>
+                    <input className="Register-password" type="password" placeholder="Password" ref={el => this.element2 = el}/>
+                    <button  className="Register-button" type="submit">Register</button>
+                </form>
+            </div>
+        )
+    }
+}
+export  default  Register;
