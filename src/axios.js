8ba4c1b25a28478c6d49cec84560e1ee67b3d700
asma <as.ch.2280@gmail.com>
2022-02-16 16:42:36 +0330

    Add axios.js

---
+++ src/axios.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+const Axios = axios.create({
+    baseURL: 'http://localhost',
+});
+Axios.defaults.withCredentials = true;
+
+export default Axios;
+
+// Axios.get('/sanctum/csrf-cookie').then( (res => {
+//     console.log(res);
+// }))
