35fcb5168ed06eba9d48f531cda57f157bd87076
asma <as.ch.2280@gmail.com>
2022-02-20 20:00:42 +0330

    Add Message

---
+++ src/Message.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Moment from "react-moment";
+class Message extends React.Component{
+
+    constructor(props) {
+        super(props);
+        this.state={date:new Date(),text:"salam"};
+    }
+    render() {
+
+        return(
+        <div className="Message">
+            <p className="Message-text">{this.props.text}</p>
+            <Moment className="Message-time" format="hh:mm">{this.state.date}</Moment>
+        </div>)
+    }
+
+}
+export default Message;
